8001f257e27a3be125b650da9fbded344e9178b4
Hroldddp <121552846+Hroldddp@users.noreply.github.com>
2024-10-23 18:07:12 +0800

    Delete osu!leaderboards directory

--- osu!leaderboards/background.js
+++
@@ -1,5 +0,0 @@
-chrome.commands.onCommand.addListener(function(command) {
-  if (command === "open-osu-game") {
-    chrome.tabs.create({ url: "https://osu.ppy.sh/rankings/osu/performance" });
-  }
-});
